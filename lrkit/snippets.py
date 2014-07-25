4f52f0dc81de62eb3367dddc3553bed1119d2d97
Henri Tuhola <cheery@boxbase.org>
2014-07-25 19:06:27 +0300

    small bug fix

--- lrkit/snippets.py
+++ lrkit/snippets.py
@@ -1,13 +1,14 @@
 def snippet(start, stop, source):
     cut1 = max(0, source.rfind('\n', 0, start)+1)
-    cut2 = source.find('\n', start, stop)
+    cut2 = source.find('\n', stop)
     if cut2 == -1:
         cut2 = len(source)
     prefix  = source[cut1:start]
     marker  = source[start:stop]
     postfix = source[stop:cut2]
     snip    = ''.join((prefix, marker, postfix)).strip()
-    lastl   = source.rfind('\n', cut1, cut2-1)+1
+    lastl   = source.rfind('\n', cut1, cut2-1)+1 + cut1
+    print cut1, start, stop, lastl, cut2
     if start < stop:
         stop -= 1
     return '\n'.join((
